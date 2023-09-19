f2576dd112c98b3f11f341fa810d3f2459c131c2
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-09-19 22:21:26 +0200

    5

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,8 @@ const inputLine = document.querySelector("#name-input");
 const helloNameLine = document.querySelector("#name-output");
 
 inputLine.addEventListener("input", (event) => {
-  helloNameLine.textContent = event.currentTarget.value;
-  //   event.currentTarget.value.length === 0
-  //     ? (helloNameLine.textContent = "Anonymous")
-  //     : (helloNameLine.textContent = event.currentTarget.value); чи доречно таке писати?
+  // helloNameLine.textContent = event.currentTarget.value;
+  event.currentTarget.value.length === 0
+    ? (helloNameLine.textContent = "Anonymous")
+    : (helloNameLine.textContent = event.currentTarget.value);
 });
