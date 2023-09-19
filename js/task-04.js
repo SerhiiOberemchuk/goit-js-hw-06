77ee031d717dbf1802121fa271b80db709337ab8
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-09-19 22:17:33 +0200

    4+5

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,10 @@
-const buttonDecrement = document.querySelector("button[data-action=decrement]");
-const buttonIncrement = document.querySelector("button[data-action=increment]");
+const buttonDecrement = document.querySelector("[data-action='decrement']");
+const buttonIncrement = document.querySelector("[data-action='increment']");
 const spanItem = document.querySelector("#value");
 
-buttonDecrement.addEventListener("click", minusNumber);
-buttonIncrement.addEventListener("click", plusNumber);
-
-function minusNumber() {
-  const currentValue = Number(spanItem.textContent);
-  spanItem.textContent = currentValue + Number(buttonDecrement.textContent);
-}
-function plusNumber() {
-  const currentValue = Number(spanItem.textContent);
-  spanItem.textContent = currentValue + Number(buttonIncrement.textContent);
-}
+buttonDecrement.addEventListener("click", () => {
+  spanItem.textContent = Number(spanItem.textContent) - 1;
+});
+buttonIncrement.addEventListener("click", () => {
+  spanItem.textContent = Number(spanItem.textContent) + 1;
+});
