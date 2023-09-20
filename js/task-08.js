bd16f8090738977a7fe96362a3bc0d0f59ef3f9a
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-09-20 22:37:37 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", getSubmitForm);
 
+function getSubmitForm(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (email.length === 0 || password.length === 0) {
+    alert("You have a blank field");
+  } else {
+    const userData = { email: `${email}`, password: `${password}` };
+    console.log(userData);
+  }
+  form.reset();
+}
