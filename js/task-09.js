69182ac1952b80804b8cb546ac2e182ef1a8c3e9
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-09-20 23:05:22 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const nameColor = document.querySelector(".color");
+const buttonChange = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+buttonChange.addEventListener("click", changeBodyColor);
+
+function changeBodyColor() {
+  const value = getRandomHexColor();
+  nameColor.textContent = value;
+  body.style.backgroundColor = value;
+}
