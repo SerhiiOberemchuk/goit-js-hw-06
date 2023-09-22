26fedc724f0a2c7f4576a4711d232741452d9be9
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-09-22 10:01:42 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controlBox = document.querySelector("#controls");
+const inputFild = controlBox.firstElementChild;
+
+// --- Get amount ---
+let amount;
+inputFild.addEventListener("input", getInputNumber);
+function getInputNumber(event) {
+  amount = Number(event.currentTarget.value);
+}
+
+// --- Create boxes ---
+const buttonCreate = controlBox.querySelector("[data-create]");
+buttonCreate.addEventListener("click", createBoxes);
+const mainBox = document.querySelector("#boxes");
+function createBoxes() {
+  const marcap = [];
+  for (let index = 0; index < amount; index++) {
+    const widthBox = 30 + 10 * index;
+    const heightBox = 30 + 10 * index;
+    marcap.push(
+      `<div style="width: ${widthBox}px; height: ${heightBox}px; background-color: ${getRandomHexColor()};"></div>`
+    );
+  }
+  mainBox.innerHTML = marcap.join("");
+}
+// --- Destroy boxes ---
+const buttonDestroy = controlBox.querySelector("[data-destroy]");
+buttonDestroy.addEventListener("click", destroyBoxes);
+function destroyBoxes() {
+  mainBox.innerHTML = "";
+}
